77e456a3c637f7f4ed8d615c1dc2d7a7eb8ddf28
Daryll28 <daryllgrainger28@gmail.com>
2022-02-03 14:18:31 +0200

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="block" v-if="showBlock" @click="stopTimer">
+restart over
+</div>
+
+</template>
+
+<script>
+export default{
+
+
+    props: ['delay'],
+    data() {
+        return{
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+           
+        },  this.delay)
+},
+
+
+methods: {
+    startTimer() {
+        this.timer = setInterval(() => {
+            this.reactionTime += 10
+        }, 10)
+    },
+    stopTimer() {
+        clearInterval(this.timer)
+        this.$emit('end', this.reactionTime)
+    }
+},
+}
+
+</script>
+
+
+
+<style>
+.block{
+    width: 350px;
+    border-radius:20px;
+    background: #ff0000;
+    color:rgb(12, 6, 6);
+    text-align: center;
+    padding:100px 0;
+    margin: 40px auto;
+    text-decoration: none;
+    font-size: 30px;
+    cursor: pointer;
+    border: 5px solid rgb(0, 0, 0);
+    
+};
+</style>
+
